aacd331fe5b7251c32d4139158b9a49335b254c9
MatheussilvaDev <matheushenrique2112@outlook.com>
2026-06-25 23:06:43 -0300

    First Commit: Inicialização do Projeto com dependências iniciais que serão utilizadas e aplicando arquitetura em camadas

---
+++ src/main/java/com/mathdev/personal_finance_api/PersonalFinanceApiApplication.java
@@ -0,0 +1,11 @@
+package com.mathdev.personal_finance_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceApiApplication.class, args);
+	}
+}
